f2958e89c1fcaa98fe56f8c6cc4438c2b7d2097e
bubblegum <120136010+suspectedesp@users.noreply.github.com>
2024-09-13 23:45:17 +0200

    added panel functions
    check_permissions, check_account, check_2fa, enable_2fa, disable_2fa

--- src/bot_hosting_wrapper/panel.py
+++ src/bot_hosting_wrapper/panel.py
@@ -3,6 +3,7 @@ import requests
 
 
 # https://control.bot-hosting.net/account/api
+# used api docs: https://dashflo.net/docs/api/pterodactyl/v1/
 # max usage: 240 requests per minute
 class Panel:
     def __init__(self, api_key, server_id=None):
@@ -10,7 +11,10 @@ class Panel:
         self.server_id = server_id
         self.page = None
         self.urls = {
-            "server_list": f"https://control.bot-hosting.net/api/client?page={self.page}"
+            "server_list": f"https://control.bot-hosting.net/api/client?page={self.page}",
+            "permission_check": "https://control.bot-hosting.net/api/client/permissions",
+            "account_check": "https://control.bot-hosting.net/api/client/account",
+            "2fa": "https://control.bot-hosting.net/api/two-factor"
         }
 
     def get_serverlist(self, page=1):
@@ -117,5 +121,101 @@ class Panel:
         response = requests.get(url=serverfiles_url, headers=headers, json=payload, timeout=6)
         if response.status_code == 200:
             return response.json()
+        else:
+            return f"{response.status_code} : {response.content}"
+
+    def check_permissions(self):
+        headers = {
+            "Accept": "application/json",
+            "Content-Type": 'application/json',
+            "Authorization": f"Bearer {self.api_key}"
+        }
+
+        response = requests.get(url=self.urls["permission_check"], headers=headers)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return f"{response.status_code} : {response.content}"
+        
+    def check_account(self):
+        headers = {
+            "Accept": "application/json",
+            "Content-Type": 'application/json',
+            "Authorization": f"Bearer {self.api_key}"
+        }
+        response = requests.get(url=self.urls["account_check"], headers=headers)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return f"{response.status_code} : {response.content}"
+        
+    def check_2fa(self):
+        """
+        Generates a TOTP QR code image to allow the setup of 2FA
+        """
+        headers = {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.api_key}"
+        }
+        response = requests.get(url=self.urls["2fa"], headers=headers)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return f"{response.status_code} : {response.content}"
+
+    def enable_2fa(self, totp_code):
+        """
+        Enables TOTP 2FA by sending a POST request to the provided URL.
+        
+        Args:
+            totp_code (str): The Time-based One-Time Password (TOTP) generated from GET /account/two-factor or the check-2fa function.
+        
+        Returns:
+            dict or str: The JSON response if successful, or an error message with the status code and content.
+        """
+        headers = {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.api_key}"
+        }
+        body = {
+            "code": totp_code
+        }
+
+        response = requests.post(url=self.urls["2fa"], headers=headers, json=body)
+        
+        if response.status_code == 200:
+            return response.json()
+        elif response.status_code == 400:
+            return f"Invalid TOTP Token: {response.content}"
+        else:
+            return f"{response.status_code} : {response.content}"
+        
+    def disable_2fa(self, password):
+        """
+        Enables TOTP 2FA by sending a POST request to the provided URL.
+        
+        Args:
+            password (str): Existing Password
+        
+        Returns:
+            bool or dict: true if successful, or an error message with the status code and content.
+        """
+        headers = {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.api_key}"
+        }
+        body = {
+            "password": password
+        }
+
+        response = requests.delete(url=self.urls["2fa"], headers=headers, json=body)
+        
+        if response.status_code == 200:
+            return True
+        elif response.status_code == 400:
+            return f"BadRequestHttpException: {response.content}"
         else:
             return f"{response.status_code} : {response.content}"
