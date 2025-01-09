4afde122d29b7b34761df18844ecbd094bf6f474
vortexdel <120136010+suspectedesp@users.noreply.github.com>
2025-01-09 19:09:10 +0100

    Update __init__.py

--- src/bot_hosting_wrapper/__init__.py
+++ src/bot_hosting_wrapper/__init__.py
@@ -5,7 +5,7 @@ from .panel import *
 ############# bot-hosting-wrapper files
 import logging
 
-LOGGING_ENABLED = True  # You can later set this from your own program (e.g. bot-hosting-wrapper.LOGGING_ENABLED = True)
+LOGGING_ENABLED = True  # You can later set this from your own program (e.g. bot_hosting_wrapper.LOGGING_ENABLED = True)
 
 if LOGGING_ENABLED:
     logging.basicConfig(level=logging.INFO)
